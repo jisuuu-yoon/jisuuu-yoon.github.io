3d958c39236d3575df21ef65a13aeded4dbda50b
Jisu <dbswltn111@gmail.com>
2019-10-08 18:14:56 +0900

    my first blog

---
+++ config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  url: 'https://jisuuu-yoon.github.io/',
+  pathPrefix: '/',
+  title: 'Blog by Jisu Yoon',
+  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Jisu Yoon',
+    photo: '/photo.jpg',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    contacts: {
+      email: 'dbswltn111@gmail.com',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: ''
+    }
+  }
+};
